d2f39229a20a2a2d16f8368924168f03faf36dbd
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-03-23 20:42:25 +0200

    release task_1 class Engineer.cs

--- Lab5CSharp/task_1/Engineer.cs
+++ Lab5CSharp/task_1/Engineer.cs
@@ -1,6 +1,19 @@
 namespace User.task_1;
 
-public class Engineer
+public class Engineer : Kadry
 {
+    private string speciality;
+    private string category;
     
+    public Engineer(string Name, string Surname, int Age, string Speciality, string Category) : base(Name, Surname, Age)
+    {
+        speciality = Speciality;
+        category = Category;
+    }
+    
+    public override void Show()
+    {
+        base.Show();
+        Console.WriteLine($"Speciality: {speciality}, Category: {category}");
+    }
 }
