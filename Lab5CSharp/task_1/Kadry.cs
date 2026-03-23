eea76efd88c75c988d3f21fcf3eba3bf9193b143
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-03-23 20:30:36 +0200

    release task_1 class Kadry.cs

--- Lab5CSharp/task_1/Kadry.cs
+++ Lab5CSharp/task_1/Kadry.cs
@@ -2,6 +2,19 @@ namespace User.task_1;
 
 public class Kadry
 {
-    
-    
+    protected string name;
+    protected string surname;
+    protected int age;
+
+    public Kadry(string Name, string Surname, int Age)
+    {
+        Name = name;
+        Surname = surname;
+        Age = age;
+    }
+
+    public virtual void Show()
+    {
+        Console.WriteLine($"Name: {name}, Surname: {surname}, Age: {age}");
+    }
 }
